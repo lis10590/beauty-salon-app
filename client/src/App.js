38f9a2f42536a4e13197d23d2ed0b20c04b78f31
Lis Setgasy <Lis@collplant-ltd.local>
2022-03-08 17:15:34 +0200

    rendered a basic calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import Home from "./components/pages/Home";
+
 import Login from "./components/Login";
 import Register from "./components/Register";
 import "bulma/css/bulma.min.css";
@@ -10,32 +9,16 @@ import NavbarComp from "./components/NavbarComp";
 import Welcome from "./components/pages/Welcome";
 
 function App() {
-  // // const calendarRef = useRef();
 
-  // const handleDateClick = (event) => {
-  //   const title = prompt("Enter an event title");
-
-  //   let calendarApi = event.view.calendar;
-
-  //   calendarApi.addEvent({
-  //     title,
-  //     start: event.startStr,
-  //     end: event.endStr,
-  //   });
-  // };
   return (
     <div className="App">
-      {/* <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        dateClick={handleDateClick}
-        // ref={calendarRef}
-      /> */}
+    
       <NavbarComp />
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="home" element={<Home/>} />
       </Routes>
     </div>
   );
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,3 +1,37 @@
-const Home = () => {};
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useRef } from "react";
+
+
+
+const Home = () => {
+
+      const calendarRef = useRef();
+
+  const handleDateClick = (event) => {
+    const title = prompt("Enter an event title");
+
+    let calendarApi = event.view.calendar;
+
+    calendarApi.addEvent({
+      title,
+      start: event.startStr,
+      end: event.endStr,
+    });
+  };
+
+  return(
+         <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        dateClick={handleDateClick}
+         ref={calendarRef} />
+
+  )
+      
+  
+
+};
 
 export default Home;
