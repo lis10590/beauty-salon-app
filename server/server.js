f611ba7091ac6d0ca71389101389b958ee97dfca
Lis Setgasy <lisset10590@gmail.com>
2022-05-30 00:23:05 +0300

    made changes for heroku deployment

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const router = express.Router();
 const routes = require("./routes");
+const path = require("path");
 require("./database");
 
 const PORT = process.env.PORT || 3001;
@@ -19,6 +20,9 @@ app.use(
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 app.use("/", routes);
 app.listen(PORT, () => {
