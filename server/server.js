de07822202e45475dab7a0e9b9e003dbd7e95349
Lis Setgasy <lisset10590@gmail.com>
2022-06-03 20:27:41 +0300

    moved package json

--- server/server.js
+++ server/server.js
@@ -19,9 +19,11 @@ app.use(
 
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    );
   });
 }
 app.use("/", routes);
